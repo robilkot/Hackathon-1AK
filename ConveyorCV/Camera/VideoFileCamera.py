01736c475aa86864b0f3c0117ab22517c463b411
robilkot <timur.robilko@gmail.com>
2025-05-03 18:30:21 +0300

    Performance imporvements

--- ConveyorCV/Camera/VideoFileCamera.py
+++ ConveyorCV/Camera/VideoFileCamera.py
@@ -1,3 +1,5 @@
+import logging
+
 import cv2
 import time
 import os
@@ -5,6 +7,7 @@ from dotenv import load_dotenv
 
 from Camera.CameraInterface import CameraInterface
 
+logger = logging.getLogger(__name__)
 load_dotenv()
 
 class VideoFileCamera(CameraInterface):
@@ -19,6 +22,10 @@ class VideoFileCamera(CameraInterface):
         self._current_frame = 0
         self._start_frame = start_frame
         self._start_time = start_time
+
+        self.__last_debug_time = time.time()
+        self.__frames_sent = 0
+        self.__debug_interval = 20  # Seconds
 
     def connect(self, max_retries=3):
         if not self.is_connected:
@@ -67,21 +74,29 @@ class VideoFileCamera(CameraInterface):
                 return None
 
         current_time = time.time()
-        time_diff = current_time - self._last_frame_time
-        frames_to_advance = int(time_diff / self._frame_duration)
 
-        if frames_to_advance < 1:
-            return None
+        debug_time_diff = current_time - self.__last_debug_time
+        if debug_time_diff > self.__debug_interval:
+            logger.info(f"read {self.__frames_sent} frames in {self.__debug_interval} ({self.__frames_sent / self.__debug_interval} fps)")
+            self.__frames_sent = 0
+            self.__last_debug_time = current_time
 
-        self._current_frame += frames_to_advance
-
-        if self._current_frame >= self._total_frames:
-            self._current_frame = 0
-            self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        else:
-            self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, self._current_frame)
+        # time_diff = current_time - self._last_frame_time
+        # frames_to_advance = int(time_diff / self._frame_duration)
+        #
+        # if frames_to_advance < 1:
+        #     return None
+        #
+        # self._current_frame += frames_to_advance
+        #
+        # if self._current_frame >= self._total_frames:
+        #     self._current_frame = 0
+        #     self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # else:
+        #     self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, self._current_frame)
 
         self._last_frame_time = current_time
+        self.__frames_sent += 1
 
         ret, frame = self.video_cap.read()
         if ret:
