d560eeb427194a3d9450246206c2e606724fadf1
Nikita <115951587+NikitaKalabin@users.noreply.github.com>
2025-05-04 11:35:43 +0300

    Update StringEqualsConverter.cs

--- ConveyorCV_frontend/ConveyorCV_frontend/Converters/StringEqualsConverter.cs
+++ ConveyorCV_frontend/ConveyorCV_frontend/Converters/StringEqualsConverter.cs
@@ -6,19 +6,22 @@ namespace ConveyorCV_frontend.Converters
 {
     public class StringEqualsConverter : IValueConverter
     {
-        public static StringEqualsConverter Instance { get; } = new StringEqualsConverter();
+        public static readonly StringEqualsConverter Instance = new();
 
-        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value == null || parameter == null)
                 return false;
 
-            return value.ToString()?.Equals(parameter.ToString(), StringComparison.OrdinalIgnoreCase) ?? false;
+            return value.ToString() == parameter.ToString();
         }
 
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is bool boolValue && boolValue && parameter != null)
+                return parameter.ToString();
+            
+            return targetType == typeof(string) ? string.Empty : null;
         }
     }
 }
